82f42843f4f0fe8222eabd834f054890027d0cd1
Alex <strelchenko.aleksandr@yandex.ru>
2026-03-05 16:45:38 +0200

    feat: implement restore task

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,35 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+const WORKSPACE_RESTORED_DIR = 'workspace_restored'
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  try {
+    const json = await fs.readFile('./snapshot.json', 'utf-8')
+  
+    const { entries } = JSON.parse(json)
+  
+    await fs.mkdir(WORKSPACE_RESTORED_DIR)
+  
+    for (const entry of entries) {
+      const { type, path: entryPath, content } = entry;
+  
+      if (type === 'file') {
+        const filePath = path.join(WORKSPACE_RESTORED_DIR, entryPath)
+
+        await fs.mkdir(path.dirname(filePath), { recursive: true })
+  
+        await fs.writeFile(filePath, Buffer.from(content, 'base64'))
+  
+      } else if (type === 'directory') {
+        const folderPath = path.join(WORKSPACE_RESTORED_DIR, entryPath)
+
+        await fs.mkdir(folderPath, { recursive: true })
+      }
+    }
+  } catch {
+    throw new Error('FS operation failed')
+  }
 };
 
 await restore();
