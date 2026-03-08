d7dfeba1b6751405c69a5556604abedb5f998c10
Alex <strelchenko.aleksandr@yandex.ru>
2026-03-08 23:00:00 +0200

    feat: implement worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,68 @@
+import * as fs from 'node:fs/promises'
+import * as os from 'node:os';
+import { Worker } from 'worker_threads';
+
 const main = async () => {
-  // Write your code here
-  // Read data.json containing array of numbers
-  // Split into N chunks (N = CPU cores)
-  // Create N workers, send one chunk to each
-  // Collect sorted chunks
-  // Merge using k-way merge algorithm
-  // Log final sorted array
+  const data = await fs.readFile('./data.json', 'utf-8');
+  const array = JSON.parse(data);
+  
+  const cpuCount = os.cpus().length;
+  
+  const chunks = [];
+  const chunkSize = Math.ceil(array.length / cpuCount)
+  
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  
+  const workers = [];
+  
+  for (let i = 0; i < chunks.length; i++) {
+    const worker = new Worker(new URL('./worker.js', import.meta.url));
+    
+    worker.postMessage(chunks[i])
+    
+    workers.push(worker)
+  }
+  
+  const results = await Promise.all(workers.map((worker) => {
+    return new Promise((resolve) => {
+      worker.on('message', (msg) => resolve(msg))
+    })
+  }))
+  
+  console.log(mergedArrays(results));
 };
 
 await main();
+
+
+function mergedArrays(array) {
+  const result = [];
+  const indexes = new Array(array.length).fill(0)
+
+  while (true) {
+    let minValue = Infinity;
+    let minIndex = -1;
+
+    for (const [i, arr] of array.entries()) {
+      const index = indexes[i];
+
+      if (index < arr.length && arr[index] < minValue) {
+        minValue = arr[index];
+        minIndex = i;
+      }
+    }
+
+    if (minIndex === -1) {
+      break;
+    }
+
+    result.push(minValue);
+    indexes[minIndex]++;
+  }
+
+  return result;
+}
+
+
