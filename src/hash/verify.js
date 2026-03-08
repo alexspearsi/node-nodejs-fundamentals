516ebdb2caac8c785d9b55370a72a9f35d873085
Alex <strelchenko.aleksandr@yandex.ru>
2026-03-08 12:41:55 +0200

    feat: implement verify task

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,35 @@
+import * as fs from 'node:fs/promises'
+import { createReadStream } from 'node:fs';
+import * as crypto from 'node:crypto'
+
 const verify = async () => {
-  // Write your code here
-  // Read checksums.json
-  // Calculate SHA256 hash using Streams API
-  // Print result: filename — OK/FAIL
+  try {
+    await fs.stat('./checksums.json')
+  } catch {
+    throw new Error('FS operation failed')
+  }
+
+  try {
+    const content = await fs.readFile('checksums.json', 'utf-8');
+  
+    const objWithHashes = JSON.parse(content);
+  
+    for (const [file, hashedString] of Object.entries(objWithHashes)) {
+  
+      const hash = crypto.createHash('sha256');
+      const stream = createReadStream(file);
+  
+      for await (const chunk of stream) {
+        hash.update(chunk)
+      }
+
+      const actualHash = hash.digest('hex');
+      console.log(`${file}: ${actualHash === hashedString ? 'OK' : 'FAIL'}`);
+    }
+    
+  } catch {
+    throw new Error('FS operation failed')
+  }
 };
 
 await verify();
